cb04d651b3da85e423bc29048c84d04317ef7af2
faizanoor3001 <33007795+faizanoor3001@users.noreply.github.com>
2018-04-22 16:41:39 +0530

    Adding full restaurant menu website

---
+++ Flask-project-2/Restaurant.py
@@ -0,0 +1,124 @@
+from flask import Flask , render_template , request, redirect, url_for, flash, jsonify
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, Menu
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/restaurants/JSON')
+def restaurantsJSON():
+	restaurantList = session.query(Restaurant).all()
+	return jsonify(Restaurant = [restaurant.serialize for restaurant in restaurantList])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(Menu).filter_by(restaurant_id = restaurant.id).all()
+	return jsonify(Menu = [i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def restaurantMenuItemJSON(restaurant_id,menu_id):
+	item = session.query(Menu).filter_by(id = menu_id).one()
+	return jsonify(Menu = item.serialize)
+
+@app.route('/')
+@app.route('/restaurants/')
+def restaurants():
+	restaurants = session.query(Restaurant).all()
+	return render_template('RestaurantAll_template.html', restaurants = restaurants)
+
+@app.route('/restaurants/new', methods=['GET','POST'])
+def newRestaurant():
+	if request.method == 'POST':
+		newRestaurantData = Restaurant(name = request.form['restaurantName'])
+		session.add(newRestaurantData)
+		session.commit()
+		flash("New Restaurant Created")
+		return redirect(url_for('restaurants'))
+	else:
+		return render_template('newRestaurant_template.html')
+
+@app.route('/restaurants/<int:restaurant_id>/edit/' , methods = ['GET','POST'])
+def editRestaurant(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	if request.method == 'POST':
+		restaurant.name = request.form['restaurantName']
+		session.add(restaurant)
+		session.commit()
+		flash("Restaurant Successfully Edited")
+		return redirect(url_for('restaurants'))
+	else:
+		return render_template('editRestaurant_template.html', restaurant = restaurant)
+
+@app.route('/restaurants/<int:restaurant_id>/delete/', methods = ['GET','POST'])
+def deleteRestaurant(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	if request.method == 'POST':
+		session.delete(restaurant)
+		session.commit()
+		flash("Deleted the restaurant successfully")
+		return redirect(url_for('restaurants'))
+	else:
+		return render_template('deleteRestaurant_template.html', restaurant = restaurant)
+
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id): 	
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(Menu).filter_by(restaurant_id = restaurant_id)
+	return render_template('Menu_template.html', restaurant = restaurant , items = items)
+
+#Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/new/', methods = ['POST','GET'])
+def newMenuItem(restaurant_id):
+	if request.method == 'POST':
+		newMItem = Menu(name = request.form['name'] ,course = request.form['course'],
+			description = request.form['description'],price = request.form['price'],
+			restaurant_id = restaurant_id)
+		session.add(newMItem)
+		session.commit()
+		flash("New Menu Item Created!")
+		#After creating a new menuItem t should redirect to the home page 
+		return redirect(url_for('restaurantMenu',restaurant_id = restaurant_id))
+	else:
+		return render_template('newMenuItem_template.html', restaurant_id = restaurant_id)
+
+#Task 2: Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET','POST'])
+def editMenuItem(restaurant_id, menu_id):
+	menuItem = session.query(Menu).filter_by(id = menu_id).one()
+	if request.method == 'POST':
+		menuItem.name = request.form['newMenuItemName']
+		session.add(menuItem)
+		session.commit()
+		flash("Menu Item successfully edited!")
+		return redirect(url_for('restaurantMenu',restaurant_id = restaurant_id))
+	if request.method == 'GET':
+		#item = session.query(Menu).filter_by(id = menu_id).one()
+		return render_template('editmenuitem.html', restaurant_id = menuItem.restaurant_id , 
+			menu_id = menuItem.id , item = menuItem)
+	
+
+#Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET','POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+	itemToDelete = session.query(Menu).filter_by(id = menu_id).one()
+	if request.method == 'POST':
+		session.delete(itemToDelete)
+		session.commit()
+		flash("Menu Item successfully Deleted")
+		return redirect(url_for('restaurantMenu', restaurant_id = itemToDelete.restaurant_id))
+	else:
+		return render_template('deleteMenuItem.html',item = itemToDelete)
+
+if __name__ == '__main__':
+	app.secret_key = "some_secret_key"
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
---
+++ Flask-project-2/database_setup.py
@@ -0,0 +1,57 @@
+# provides a lots of functions which lets connect with the cmd
+import sys
+
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    name = Column(String(80) ,nullable = False)
+    id = Column(Integer , primary_key = True)
+
+    @property
+    def serialize(self):
+        return {
+            'name' :self.name,
+            'id' : self.id
+        }
+        pass
+
+
+class Menu(Base):
+    __tablename__ = 'menu_item'
+    name = Column(String(120) , nullable = False)
+    id = Column(Integer , primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer , ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        #returns an object in easily serializable format
+        return {
+            'name' : self.name,
+            'description' : self.description,
+            'id' : self.id,
+            'price' : self.price,
+            'course' : self.course
+        }
+        pass
+
+### insert at the end of the
+
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
